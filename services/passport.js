fbfd372817bd93c3c0b05ceb45512898dee61c04
Mehak Saini <mehak@192.168.1.7>
2021-02-28 10:31:38 +0530

    initial commit

---
+++ services/passport.js
@@ -0,0 +1,15 @@
+const passport=require('passport');
+const GoogleStrategy=require('passport-google-oauth20').Strategy;
+const keys=require('../config/keys')
+
+passport.use(new GoogleStrategy({
+    clientID:keys.googleClientID,
+    clientSecret:keys.googleClientSecret,
+    callbackURL:'/auth/google/callback'
+}, (accessToken, second, profile)=>{
+    console.log('accessToken '+accessToken)
+    console.log('second '+second)
+    console.log('profile '+profile)
+    //console.log('fourth '+fourth)
+
+}));
